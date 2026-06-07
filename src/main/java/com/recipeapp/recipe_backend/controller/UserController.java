da470922066992ab574314fe0aa3781c6cdaa085
Gherman Larisa <gherman.larisa3@gmail.com>
2026-06-07 10:34:35 +0300

    refractor: change usercontroller endpoint name to users

--- src/main/java/com/recipeapp/recipe_backend/controller/UserController.java
+++ src/main/java/com/recipeapp/recipe_backend/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 @AllArgsConstructor
 public class UserController {
     private final UsersService usersService;
